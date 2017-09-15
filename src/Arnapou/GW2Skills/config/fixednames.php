d4f3c1cd48ffebacd2dd8681756cbae3bea782af
Arnaud Buathier <arnaud.buathier@livingobjects.com>
2017-09-15 12:07:11 +0200

    added items

--- src/Arnapou/GW2Skills/config/fixednames.php
+++ src/Arnapou/GW2Skills/config/fixednames.php
@@ -12,13 +12,17 @@
         'trappers respite'       => 'trapper\'s respite',
         'illusionists celerity'  => 'illusionist\'s celerity',
     ],
+    'skills' => [
+        'light\'s judgement'     => 'light\'s judgment',
+    ],
     'buffs' => [
         'nopalitos saute'             => 'nopalitos sauté',
         'plate of lemongrass poultry' => 'plates of lemongrass poultry',
         'strawberries and biscuts'    => 'strawberries and biscuits',
     ],
     'upgrades' => [
-        'of flame legion' => 'of the flame legion',
-        'of the rata sum' => 'of rata sum',
+        'of flame legion'                  => 'of the flame legion',
+        'of the rata sum'                  => 'of rata sum',
+        'exquisite freshwater pearl jewel' => 'exquisite freshwater pearl'
     ],
 ];
